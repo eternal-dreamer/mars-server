82dfb209d836c9b570bd3bbf2617f884352fd51d
Shrishti <shrishtigoyal2608@gmail.com>
2023-03-04 15:00:45 +0530

    backend setup

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://shrishtigoyal2608:w8Hids6sXRYWub2Q@cluster0.emmcfo0.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set("strictQuery", false);
+
+
