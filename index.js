585fae79d6f53616c682c1e3b9af4f88f65bcaa9
Shrishti <shrishtigoyal2608@gmail.com>
2023-03-05 05:20:13 +0530

    auth setup

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
+import userRoutes from './routes/users.js';
 import cors from "cors";
 
 const app = express();
@@ -8,6 +9,8 @@ const app = express();
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
+
+app.use('/user', userRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://shrishtigoyal2608:w8Hids6sXRYWub2Q@cluster0.emmcfo0.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
---
+++ models/user.js
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from 'mongoose';
+
+const userSchema = mongoose.Schema({
+    fullName: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true },
+    phone: { type: String, required: true },
+    password: { type: String, required: true },
+    birthDate: { type: Date, default: new Date() },
+    gender: String,
+    maritialStatus: String,
+    imageUrl: String,
+    
+    id: { type: String },
+    
+  });
+
+  const User = mongoose.model('User', userSchema);
+
+export default User;
