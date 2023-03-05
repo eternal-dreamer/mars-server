b8ca1d243bdefd3a6959f7c88bb41530347bcd6e
Mansi Sharma <mansi.isha.mansi@gmail.com>
2023-03-05 07:04:56 +0530

    update

--- models/user.js
+++ models/user.js
@@ -1,21 +1,20 @@
 import mongoose, { Schema } from 'mongoose';
 
 const userSchema = mongoose.Schema({
-    fullName: { type: String, required: true },
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    email: { type: String, required: true },
-    phone: { type: String, required: true },
-    password: { type: String, required: true },
-    birthDate: { type: Date, default: new Date() },
-    gender: String,
-    maritialStatus: String,
-    imageUrl: String,
-    
-    id: { type: String },
-    
-  });
+  fullName: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  birthDate: { type: Date, default: new Date() },
+  gender: String,
+  maritialStatus: String,
+  imageUrl: String,
 
-  const User = mongoose.model('User', userSchema);
+  id: { type: String },
+
+});
+
+const User = mongoose.model('User', userSchema);
 
 export default User;
