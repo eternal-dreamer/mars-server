60063f97bd7f393d9fb4083e8f145ed8e36b9282
Mansi Sharma <mansi.isha.mansi@gmail.com>
2023-03-05 06:20:01 +0530

    updated

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
-import express from "express";
-import mongoose from "mongoose";
-import bodyParser from "body-parser";
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
 import userRoutes from './routes/users.js';
-import cors from "cors";
 
 const app = express();
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-
 app.use('/user', userRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://shrishtigoyal2608:w8Hids6sXRYWub2Q@cluster0.emmcfo0.mongodb.net/?retryWrites=true&w=majority';
-const PORT = process.env.PORT|| 5000;
+app.get('/', (req, res) => {
+  res.send('Hello to Mars API');
+});
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-  .catch((error) => console.log(`${error} did not connect`));
+const CONNECTION_URL = "mongodb+srv://shrishtigoyal2608:w8Hids6sXRYWub2Q@cluster0.emmcfo0.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
 
 mongoose.set("strictQuery", false);
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+  .catch((error) => console.log(error.message));
 
 
